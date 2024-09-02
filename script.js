2c0807b4a8562922af8bf8d183ded6644f8afab5
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-09-02 09:12:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+document.getElementById('button').addEventListener('click', function() {
+    const name = document.getElementById('name').value;
+    const year = document.getElementById('year').value;
+    
+    let newUrl = "https://localhost:8080/";
+
+    if (name) {
+        newUrl += `?name=${encodeURIComponent(name)}`;
+        if (year) {
+            newUrl += `&year=${year}`;
+        }
+    } else if (year) {
+        newUrl += `?year=${year}`;
+    }
+
+    document.getElementById('url').textContent = newUrl;
+});
